c24439e8e354a25c3a119cadf18ab63a35fbb0ba
sofiasolomon0914 <ssolomon2770@eagle.fgcu.edu>
2019-12-06 10:19:17 -0500

    Clock Display

--- Main.py
+++ Main.py
@@ -2,3 +2,20 @@
 This program will allow user to set an alarm that will go off with a sounds and also allow a message to display while
 the alarm is going off.
 """
+
+import sys
+from tkinter import *
+import time
+
+
+def tick():
+    time_string = time.strftime("%H:%M:$5")
+    clock.config(text=time_string)
+    clock.after(200, tick)
+
+
+root = Tk()
+clock = Label(root, font=("times", 5, "bold"), bg="white")
+clock.grid(row=0, column=1)
+tick()
+root.mainloop()
