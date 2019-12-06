c8680b071881381e2973dfa590cc5c988b931810
sofiasolomon0914 <ssolomon2770@eagle.fgcu.edu>
2019-12-06 10:30:04 -0500

    Clock Display Fixed

--- Main.py
+++ Main.py
@@ -3,19 +3,18 @@ This program will allow user to set an alarm that will go off with a sounds and 
 the alarm is going off.
 """
 
-import sys
 from tkinter import *
 import time
 
 
 def tick():
-    time_string = time.strftime("%H:%M:$5")
+    time_string = time.strftime("%H:%M:%S")
     clock.config(text=time_string)
     clock.after(200, tick)
 
 
 root = Tk()
-clock = Label(root, font=("times", 5, "bold"), bg="white")
+clock = Label(root, font=("times", 100, "bold"), bg="white")
 clock.grid(row=0, column=1)
 tick()
 root.mainloop()
